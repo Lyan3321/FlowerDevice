4b4dbddcd0770287513f1a5c3e13d233c094b405
Lyan3321 <1159660811@qq.com>
2016-09-23 16:56:02 +0800

    利用反射实现数据库的增删改查。

--- MysqlDemo/src/cn/liuyan/test01/MysqlTest.java
+++ MysqlDemo/src/cn/liuyan/test01/MysqlTest.java
@@ -8,7 +8,10 @@ public class MysqlTest {
 	public static void main(String[] args) {
 		
 		try {
-			JDBCTest.testAddNewDevice();
+//			JDBCTest.testAddNewDevice();
+			String sql = "SELECT address address,temp temp,humid humid from flowerdemo"
+					+ " WHERE address = ?";
+			JDBCTest.get(Device.class, sql, "192.168.1.1");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
